af9a331ed637abe644f490964e5b13fa4657c845
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-10-27 20:00:07 +0000

    aula.27.10.23

--- aula27.10.23.ts
+++ aula27.10.23.ts
@@ -1,60 +1,92 @@
-class Quarto {
-    constructor(public quantidade: number){}
-}
-class Banheiro{
-    constructor(public quantidade: number){}
-}
+// class Quarto {
+//     constructor(public numero: number){}
+// }
+// class Banheiro{
+//     constructor(public numero: number){}
+// }
 
-class Sala{
-    constructor(public quantidade: number){}
-}
+// class Sala{
+//     constructor(public numero: number){}
+// }
 
-class Cozinha{
-    constructor(public tamanho: number){}
-}
+// class Cozinha{
+//     constructor(public tamanho: number){}
+// }
 
-class Casa{
-    private quartos: Quarto[];
-    private banheiros: Banheiro[];
-    private salas: Sala[];
-    private cozinha: Cozinha;
+// class Casa{
+//     private quartos: Quarto[];
+//     private banheiros: Banheiro[];
+//     private salas: Sala[];
+//     private cozinha: Cozinha;
 
-    constructor(){
-        this.quartos = [new Quarto(1), new Quarto(2), new Quarto(3), new Quarto(4)];
-        this.banheiros = [new Banheiro(1), new Banheiro(2)];
-        this.salas = [new Sala(1), new Sala(2)];
-        this.cozinha = new Cozinha(100);
-    }
+//     constructor(){
+//         this.quartos = [new Quarto(1), new Quarto(2), new Quarto(3), new Quarto(4)];
+//         this.banheiros = [new Banheiro(1), new Banheiro(2)];
+//         this.salas = [new Sala(1), new Sala(2)];
+//         this.cozinha = new Cozinha(100);
+//     }
 
-    verificarQuartos(){
-        for (const quarto of this.quartos){
-            console.log(`Quarto ${quarto.quantidade} em bom estado`);
-        }
-    }
+//     verificarQuartos(){
+//         for (const quarto of this.quartos){
+//             console.log(`Quarto ${quarto.numero} em bom estado`);
+//         }
+//     }
 
-    verificarBanheiros(){
-        for (const banheiro of this.banheiros){
-            console.log(`Banheiro ${banheiro.quantidade} em bom estado`);
-        }
-    }
+//     verificarBanheiros(){
+//         for (const banheiro of this.banheiros){
+//             console.log(`Banheiro ${banheiro.numero} em bom estado`);
+//         }
+//     }
 
-    verificarSalas(){
-        for (const sala of this.salas){
-            console.log(`Sala ${sala.quantidade} em bom estado`);
-        }
-    }
+//     verificarSalas(){
+//         for (const sala of this.salas){
+//             console.log(`Sala ${sala.numero} em bom estado`);
+//         }
+//     }
 
-    verificarCozinha(){
-        console.log(`Cozinha de tamanho ${this.cozinha.tamanho} em bom estado`);
-    }
-}
+//     verificarCozinha(){
+//         console.log(`Cozinha de tamanho ${this.cozinha.tamanho} em bom estado`);
+//     }
+// }
 
-function main() {
-    const minhacasa = new Casa();
-    minhacasa.verificarQuartos();
-    minhacasa.verificarBanheiros();
-    minhacasa.verificarSalas();
-    minhacasa.verificarCozinha();
-}
+// function main() {
+//     const minhacasa = new Casa();
+//     minhacasa.verificarQuartos();
+//     minhacasa.verificarBanheiros();
+//     minhacasa.verificarSalas();
+//     minhacasa.verificarCozinha();
+// }
   
+// main();
+
+export interface TipoCarro {
+    marca: string;
+    modelo: string;
+}
+
+export class Carro {
+    private tipoCarro: TipoCarro;
+  
+    constructor(tipoCarro: TipoCarro) {
+        this.tipoCarro = tipoCarro;
+    }
+}
+
+class Sedan implements TipoCarro{
+    constructor(public marca: string, public modelo: string){
+        console.log(`carro Sedan da marca ${this.marca} e modelo ${this.modelo}`);
+    }
+}
+
+class SUV implements TipoCarro{
+    constructor(public marca: string, public modelo: string){
+        console.log(`carro SUV da marca ${this.marca} e modelo ${this.modelo}`);
+    }
+}
+
+function main(){
+    const meucarro = new Carro(new Sedan("fiat", "palio"));
+    const meucarro1 = new Carro(new SUV("chevrolet", "toro"));
+}
+
 main();
